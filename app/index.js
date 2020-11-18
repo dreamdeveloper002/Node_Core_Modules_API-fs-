990cca8aaf227b1346daf5c0bf48fbb8c4b4d078
dreamdeveloper002 <bkosemani1@gmail.com>
2020-11-18 20:38:50 +0100

    config env

--- app/index.js
+++ app/index.js
@@ -3,7 +3,9 @@
 //Dependencies
 const http = require('http'),
       url = require('url'),
-      stringDecoder = require('string_decoder').stringDecoder;
+      stringDecoder = require('string_decoder').StringDecoder,
+      config = require('./config');
+
 
 //The server should respond to all requests with a string
 const server = http.createServer(function (req, res) {
@@ -18,7 +20,7 @@ const trimmedPath = path.replace(/^\/+|\/+$/g,'');
 //Get the query string as an object
 const queryStringObject = parsedUrl.query;
 
-// Get the HTTP method
+//Get the HTTP method
 const method = req.method.toUpperCase();
 
 //Get the headers as an object
@@ -36,19 +38,68 @@ req.on('data', function(data) {
 req.on('end', function () {
    buffer = decoder.end();
 
-//Send the response
-res.end('Hello World\n');
+//handlers each request should go to, if one isn't found, use not found handler
+const choosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-//Log the request path
-console.log(`Request recieved with this payload ${buffer}`);
+//Construct data object to send to the data
+const data = {
+  'trimmedPath' : trimmedPath,
+  'queryStringObject' : queryStringObject,
+  'method' : method,
+  'headers' : headers,
+  'payload' : buffer
+};
 
+//Route the request to the router specified in the router
+choosenHandler(data, function (statusCode, payload) {
+   //Use the status code called back by the handler, or the default to 200
+  statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
    
-})
+  //Use the payload called back by the handler or default to an empty object
+  payload = typeof(payload) == 'object' ? payload : {};
+
+  // convert the payload to a string
+  const payloadString = JSON.stringify(payload);
+
+  //Return the response
+  res.setHeader('Content-Type','application/json')
+  res.writeHead(statusCode).end(payloadString);
+
+  //Log the request path
+console.log(`Returning this response : ${statusCode}, ${payloadString}`);
+
+});
+
+});
 
 });
 
 
-//Start the server, and have it listen on port 3000
-server.listen(3000, function () {
-    console.log('The server is listening on port 3000');
+//Start the server
+server.listen(config.port, function () {
+    console.log(`The server is listening on port ${config.port} in ${config.envName} mode`);
 })
+
+//Define the handlers
+const handlers = {};
+
+
+//Sample handler
+handlers.sample = function (data, callback) {
+  
+//Callback http status code, and a payload object;
+  callback(406, {'nama': 'my name is sample handler'})
+};
+
+//Not found handler
+handlers.notFound = function (data, callback) {
+  callback(404);
+} 
+
+
+
+//Define a request router
+const router = {
+  'sample' : handlers.sample
+};
+
