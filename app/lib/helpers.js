4471f2b2822ec2602774f7d0bb768e824bc4c2f9
dreamdeveloper002 <bkosemani1@gmail.com>
2020-12-14 23:48:23 +0100

    twilio helper function

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -2,7 +2,9 @@
 
 // Dependencies
 const crypto = require('crypto'),
-      config = require('../config');
+      config = require('../config'),
+      querystring = require(querystring),
+      https = require(https);
 
 
 // Container for all the helpers
@@ -60,6 +62,66 @@ helpers.createRandomString = function(strLength) {
      return false;
    }
 
+}
+
+//Send an sms message via twilio
+helpers.sendTwilioSms = function (phone,msg,callback) {
+    //validate params
+    phone = typeof(phone) == 'string' && phone.trim().length == 10  ? phone.trim() : false;
+    msg = typeof(msg) == 'string' && msg.trim().length > 0 && msg.trim().length <= 1600  ? msg.trim() : false;
+
+    if(phone && msg) {
+        
+        //Config the request payload send to twilio
+        const payload = {
+          'from' : config.twilio.fromPhone,
+          'To' : '+1'+phone,
+          'Body' : msg
+        }
+
+        //Stringify the payload
+        const stringPayload = querystring.stringify(payload);
+
+        //Configure the request details
+        const requestDetails = {
+          'protocol' : 'https',
+          'hostname' : 'api.twilio.com',
+          'method' : 'POST',
+          'path' : '/2010-04-01/Accounts/'+config.twilio.accountSid+'/Messages.json',
+          'auth' : config.twilio.accountSid+':'+config.twilio.authToken,
+          'headers' : {
+            'Content-Type' : 'application/x-ww-form-urlencoded',
+            'Content-Length' : Buffer.byteLength(stringPayload)
+          }
+        };
+
+        //Instantiate the request object
+        let req = https.request(requestDetails, function(res) {
+            //Grap the status of the sent request
+            let status = res.statusCode;
+
+            //callback success if request went through
+            if(status == 200 || status == 201) {
+              callback(false)
+            } else {
+              callback('Status code returned was '+status);
+            }
+        });
+       
+
+        req.on('error',function(e) {
+          callback(e)
+        });
+       
+        //Add the payload
+        req.write(stringPayload);
+
+        //End the request
+        req.end();
+
+    } else {
+      callback('Given parameters were missing or invalid')
+    }
 }
 
 module.exports = helpers;
